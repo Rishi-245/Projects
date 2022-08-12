11d861146b803bf6d77ef120f170ecb348f4c0b8
Rishi-245 <78450356+Rishi-245@users.noreply.github.com>
2022-08-11 21:12:46 -0400

    Create dog_years.js

---
+++ Javascript Projects/dog_years.js
@@ -0,0 +1,21 @@
+// My Age
+var myAge = 16;
+ 
+// Changing Variable
+var earlyYears = 2;
+earlyYears *= 10.5;
+ 
+// Accounted The First 2 Years Above
+var laterYears = myAge - 2;
+ 
+// Number of Dog Years Accounted For Later Years
+laterYears *= 4;
+ 
+// My Age In Dog Years
+myAgeInDogYears = earlyYears + laterYears;
+ 
+// My Name in Lower Case Letters
+var myName = "Rishi".toLowerCase();
+ 
+// Console Statement
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
