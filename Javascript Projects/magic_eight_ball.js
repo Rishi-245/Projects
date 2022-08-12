e954f0a1a3745505014e9b5c61e0ef8739213501
Rishi-245 <78450356+Rishi-245@users.noreply.github.com>
2022-08-11 21:14:28 -0400

    Update magic_eight_ball.js

--- Javascript Projects/magic_eight_ball.js
+++ Javascript Projects/magic_eight_ball.js
@@ -28,3 +28,25 @@ switch (randomNumber) {
  
  case 3:
    eightBall = "Cannot predict now";
+   break;
+ 
+ case 4:
+   eightBall = "Do not count on it";
+   break;
+ 
+ case 5:
+   eightBall = "My sources say no";
+   break;
+ 
+ case 6:
+   eightBall = "Outlook not so good";
+   break;
+ 
+ case 7:
+   eightBall = "Signs point to yes";
+   break;
+}
+ 
+// Final Print Statement
+console.log(eightBall)
+
