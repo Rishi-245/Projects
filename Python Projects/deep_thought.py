c0952f3de8ba7c59d33527669bfa903b0871569f
Rishi-245 <78450356+Rishi-245@users.noreply.github.com>
2022-08-11 21:28:49 -0400

    Create deep_thought.py

---
+++ Python Projects/deep_thought.py
@@ -0,0 +1,11 @@
+#CS50's Introduction to Programming with Python
+#Rishi Patel
+#April 22, 2022
+#Project: Deep Thought
+
+thought = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+
+if thought == "42" or thought == "forty-two" or thought == "forty two":
+    print("Yes")
+else:
+    print("No")
